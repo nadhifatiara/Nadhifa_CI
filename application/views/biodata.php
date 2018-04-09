e59d7511e85f60061c714f2da1278b93d90ef6ac
NADHIFA\Nadhifa <nadhifatiaraputri@gmail.com>
2018-04-09 18:54:05 +0700

    tugas 2

--- application/views/biodata.php
+++ application/views/biodata.php
@@ -25,6 +25,9 @@
           <li class="nav-item">
             <a class="nav-link" href="#">News</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url("index.php/Blog") ?>">Blog</a>
+          </li>
         </ul>
       </div>
     </nav>
@@ -111,12 +114,9 @@
            <?php } ?>
           </table>
         </div>
-          
         </div>
-        
       </div>
-    </main>
-																			
+    </main>															
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
