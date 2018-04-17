e544c5240492b98403e00072921e7740de05bde4
NADHIFA\Nadhifa <nadhifatiaraputri@gmail.com>
2018-04-17 20:59:02 +0700

    Tugas 3.2 delete

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -128,4 +128,9 @@ class Blog extends CI_Controller {
          } 
       }  
    }
+
+   public function delete_action($id){
+      $this->Blog_models->delete($id);
+      redirect('Blog');
+   }
 }
